1684d4dae386a015d668a61809a3c19ba058f87d
Stephen Gabriel S. Alojado <21-07082@g.batstate-u.edu.ph>
2024-02-22 10:30:54 +0800

    Updated Ass 2

--- Assignment2_2.m
+++
@@ -1,6 +0,0 @@
-%% Assignment 2 Inverse Laplace
-
-syms s t;
-F1 = sym(s^2 +2s^3+3s^2+4s+5/(s^2 +s));
-f1 = ilaplace(F1);
-pretty(f1)
--- Assignment2_3.m
+++ Assignment2_3.m
@@ -4,5 +4,3 @@ syms s t
 F1 = (s^2 + 2*s^3 + 3*s^2 + 4*s + 5)/(s^2 + s)
 f1 = ilaplace(F1)
 pretty(f1)
-
-F2 = 5(s+2)/s^2*(s+1)*(s+3)
